71e4e5267d391a1eac9b9ba77730d01d8693b9d1
y1rn <y1rn66@gmail.com>
2023-08-30 04:18:38 +0800

    update download jar logical

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,10 @@ import { HttpsProxyAgent } from 'https-proxy-agent';
 import * as net from "net";
 import { LOADIPHLPAPI } from "dns";
 import * as cp from "child_process";
-import { platform } from 'os'
 import { delimiter, sep } from 'path'
+import { platform } from 'os'
+
+import * as glob from "glob";
 
 
 
@@ -16,24 +18,24 @@ const GoogleJavaFormatInfoUrl = "https://api.github.com/repos/google/google-java
 const LocalJarFile = "google-java-format-service.jar";
 var PROCESS:cp.ChildProcessWithoutNullStreams;
 var PORT:number;
-
-
+const NAME ='y1rn.google-java-format';
+const EXE_FILE_EXT = (platform() == 'win32' ? ".exe" : "")
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-
-  if (!fs.existsSync(context.globalStorageUri.fsPath)) {
-    fs.mkdir(context.globalStorageUri.fsPath, (error: any) => error ? console.log(error) : console.log('You have created the' + context.globalStorageUri.fsPath));
-  }
-  const filePath = context.globalState.get("gjfs.jar-file");
-  if (!filePath || filePath == "" || !fs.existsSync(filePath.toString())) {
-    downloadJar(context).then(() => { 
-      starService(context);
-    })
-  }else {
-    starService(context);
+  vscode.workspace.onDidChangeConfiguration(e => {
+    const javaCfg = vscode.workspace.getConfiguration("[java]");
+    if (!!javaCfg && javaCfg['editor.defaultFormatter'] == NAME){
+      startUp(context);
+    } else {
+      deactivate();
+    }
+  })
+  const javaCfg = vscode.workspace.getConfiguration("[java]");
+  if (!!javaCfg && javaCfg['editor.defaultFormatter'] == NAME) {
+    startUp(context);
   }
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -57,13 +59,28 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
+function startUp(context: vscode.ExtensionContext){
+  
+
+  if (!fs.existsSync(context.globalStorageUri.fsPath)) {
+    fs.mkdir(context.globalStorageUri.fsPath, (error: any) => error ? console.log(error) : console.log('You have created the' + context.globalStorageUri.fsPath));
+  }
+  const filePath = context.globalState.get<string>("gjfs.jar-file");
+  if (!filePath || filePath == "" || !fs.existsSync(filePath.toString())) {
+    downloadJar(context).then(() => {
+      starService(context);
+    }).catch((a) => { })
+  } else {
+    starService(context);
+  }
+}
+
 // this method is called when your extension is deactivated
 export function deactivate() {
   if (PROCESS != null && !PROCESS.killed) {
-    console.log(`kill process  : ${PROCESS.pid}`);
-    PROCESS.kill();
     process.kill(PROCESS.pid, 'SIGINT');
-    PROCESS.disconnect();
+    // PROCESS.disconnect();
+    console.log(`kill process  : ${PROCESS.pid}`);
   }
 }
 
@@ -71,7 +88,7 @@ async function getResp(
   document: vscode.TextDocument,
   range: vscode.Range
 ): Promise<vscode.TextEdit[]> {
-  if (PORT <= 0) {
+  if (!PROCESS || !PROCESS.pid) {
     Promise.reject([])
   }
   const cfg = vscode.workspace.getConfiguration("gjfs");
@@ -94,21 +111,33 @@ async function getResp(
   return Promise.resolve([vscode.TextEdit.replace(range, data)]);
 }
 
-async function starService(context: vscode.ExtensionContext): Promise<void> {
-  const googleJarPath = context.globalState.get("gjfs.jar-file");
-  if (!googleJarPath) {
-    return Promise.reject();
-  }
+async function starService(context: vscode.ExtensionContext) {
 
-  // PORT = 8030;
+  if (PROCESS && PROCESS.pid) {
+    return;
+  }
+  
+  const googleJarPath = context.globalState.get("gjfs.jar-file");
+
+  if (!googleJarPath) {
+    return ;
+  }
 
   PORT = await getPortFree();
-  if (PORT <= 0) {
-    return Promise.reject();
-  }
+
   const localJarPath = context.extensionPath + sep + "dist" + sep + LocalJarFile;
 
-  PROCESS = cp.spawn("java", ['-cp', `\"${localJarPath}${delimiter}${googleJarPath}\"`, 
+  var cmd = "java";
+  var redhatPath = vscode.extensions.getExtension("redhat.java")?.extensionPath;
+  if (redhatPath) {
+    glob(`${redhatPath}${sep}jre${sep}*${sep}bin${sep}java${EXE_FILE_EXT}` ,(err, files)=>{
+      if (files && files.length > 0) {
+        cmd = files[0];
+      }
+    });
+  }
+
+  PROCESS = cp.spawn(cmd, ['-cp', `\"${localJarPath}${delimiter}${googleJarPath}\"`, 
     '--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED', 
     '--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED', 
     '--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED', 
@@ -120,11 +149,12 @@ async function starService(context: vscode.ExtensionContext): Promise<void> {
       shell: true,
       cwd: context.extensionPath,
     });
-  console.log(`service listening on port : ${PORT}`);
-
-  PROCESS.stderr.on('data', (data) => {
-    console.log(`google java format: ${data}`);
-  });
+  // PROCESS.stdout.on('data', (data) => {
+  //   console.log(`google java format: ${data}`);
+  // });
+  // PROCESS.stderr.on('data', (data) => {
+  //   console.log(`google java format: ${data}`);
+  // });
   
 }
 
@@ -140,30 +170,31 @@ async function downloadJar(context: vscode.ExtensionContext): Promise<void>{
     method: "GET",
     agent: agent,
   });
-  const data = await response.json();
   if (!response.ok) {
+    const data = await response.text();
     vscode.window.showErrorMessage(data);
     return Promise.reject();
   }
+  const data = await response.json();
   var  downloadUrl;
+  var filePath;
   for (const asset of data.assets) {
     if (asset.name.match("^google-java-format-[0-9]+\.[0-9]+\.[0-9]+-all-deps.jar$")) {
       downloadUrl = asset.browser_download_url;
-      context.globalState.update("gjfs.jar-file", context.globalStorageUri.fsPath + sep + asset.name);
+      filePath = context.globalStorageUri.fsPath + sep + asset.name;
       break;
     }
   }
 
-  const filePath = context.globalState.get("gjfs.jar-file");
-
   if (!filePath) return Promise.reject();
 
   if (fs.existsSync(filePath.toString())) {
+    context.globalState.update("gjfs.jar-file", filePath);
     return Promise.resolve();
   }
 
   if (downloadUrl) {
-    console.log(`download file from url [${downloadUrl}]`);
+    // console.log(`download file from url [${downloadUrl}]`);
  
     const response = await fetch(downloadUrl, { agent: agent });
     
@@ -175,7 +206,7 @@ async function downloadJar(context: vscode.ExtensionContext): Promise<void>{
     }
     const buffer = await response.buffer();
     await fs.promises.writeFile(filePath.toString(), buffer);
-    console.log(`file [${filePath}] downloaded`);
+    context.globalState.update("gjfs.jar-file", filePath);
     return Promise.resolve()
 
   }
